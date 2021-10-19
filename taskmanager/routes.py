2dcc67aa43d81033b98daba7704b78ad0bf84304
arthurvguide <arthurvguideuk@gmail.com>
2021-10-19 16:26:51 +0000

    Import datamodels to postgres database

---
+++ taskmanager/routes.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from taskmanager import app, db # noqa
+from taskmanager.models import Category, Task
+
+@app.route("/")
+def home():
+    return render_template("base.html")
